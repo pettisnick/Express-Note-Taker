afe92801c4bdfee070453dfb145524f60242dfc0
Nick Pettis <pettisnick@gmail.com>
2020-08-25 23:32:53 -0400

    delete route

--- Develop/server.js
+++ Develop/server.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+//returns a function
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
 
@@ -29,14 +30,29 @@ app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
 });
 
+
+let notes = [];
+
 //Routes
-app.get("./notes", (req, res) => {
+app.get("/notes", (req, res) => {
     //console.log(path.join(__dirname, "notes.html"));
     //path.join is joining them in one singualr path that express can read and locate in html, then send back to the browser
     res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-app.get("./*", (req, res) => {
+app.get("*", (req, res) => {
     //console.log(path.join(__dirname, "index.html"));
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+//Delete 
+//receive a parameter containing the ID of a note to delete? Give each note a unique ID when it's saved.
+//To delete a note, need to read all notes from db.json file, remove the note w/the given ID property, and rewrite the notes to db.json
+app.delete("/api/notes/:id", (req, res) => {
+    
+    for (var i = 0; i < notes.length; i++) {
+        if 
+        console.log(notes.length[i]);
+    }
+
+})
